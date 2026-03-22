044b3f4909468894a15f526b0f82969c682d9793
Uddipan Mondal <126544891+1udialways@users.noreply.github.com>
2026-03-23 02:06:41 +0530

    fixed signup ts errors

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,4 +1,73 @@
-import { CSSProperties } from "react";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import type { CSSProperties } from "react";
+import { useState } from "react";
+import { auth } from "./firebase";
+
+export default function Signup({ onLogin }: any) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await updateProfile(userCredential.user, {
+        displayName: name,
+      });
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <h2>🚀 Sign Up</h2>
+
+        <input
+          style={styles.input}
+          placeholder="Full Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          style={styles.input}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          style={styles.input}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <button style={styles.button} onClick={handleSignup}>
+          Create Account
+        </button>
+
+        <p>
+          Already have account?{" "}
+          <span style={styles.link} onClick={onLogin}>
+            Login
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
 
 const styles: { [key: string]: CSSProperties } = {
   container: {
@@ -14,7 +83,7 @@ const styles: { [key: string]: CSSProperties } = {
     padding: "30px",
     borderRadius: "16px",
     width: "320px",
-    textAlign: "center", // ✅ now works
+    textAlign: "center",
   },
   input: {
     width: "100%",
